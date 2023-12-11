7f6a64ad3a9a0970faefd970ac4ba98fe78b6e43
= <muhbintang650@gmail.com>
2023-12-11 23:52:11 +0800

    component header & footer 😍

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+  <div class="copyright">Copyright 2023 Muhammad Bintang</div>
+</footer>
+
+<style>
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+  .copyright {
+    color: #aaa;
+    font-size: 14;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<header>
+  <h1><img src="img/Ryugen.png" alt="logo" /></h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20;
+  }
+  h1 {
+    margin: 0;
+    padding: 10px 10px;
+    text-align: center;
+  }
+  img {
+    max-width: 90px;
+  }
+</style>
