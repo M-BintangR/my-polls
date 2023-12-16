f8c7a600929c9cc426b8bd615b9ef5b9988a1170
= <muhbintang650@gmail.com>
2023-12-16 10:53:54 +0800

    reusable tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
   import Footer from "./components/Footer.svelte";
   import Header from "./components/Header.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const tabChange = (e) => {
+    activeItem = e.detail;
+  };
 </script>
 
 <Header />
 <main>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, adipisci!
-  </p>
+  <Tabs on:tabChange={tabChange} {items} {activeItem} />
+  {#if activeItem == "Current Polls"}
+    <p>Poll List Component Goes Here</p>
+  {:else if activeItem == "Add New Poll"}
+    <p>Tambah Poll Baru</p>
+  {/if}
 </main>
 <Footer />
 
---
+++ src/shared/Tabs.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let items;
+  export let activeItem;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li>
+        <div
+          on:click={() => dispatch("tabChange", item)}
+          class:active={item == activeItem}
+        >
+          {item}
+        </div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .tabs {
+    margin-bottom: 40px;
+  }
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+    gap: 20px;
+  }
+  li {
+    margin: 0;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+  }
+  .active {
+    color: #d91b42;
+    border-bottom: 2px solid;
+    padding-bottom: 8px;
+  }
+</style>
